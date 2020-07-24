6b9a115beeaf6bc495afbbddce2818d8b13e91ba
Scott Bergler <commandinghands@gmail.com>
2020-07-24 14:30:08 -0700

    Display Fib Seq.

--- client/src/FibInput.js
+++ client/src/FibInput.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 export const FibInput = () => {
+  console.log('FibInput rendered');
   const [fibNum, setFibNum] = React.useState(0);
   const [fibSeq, setFibSeq] = React.useState([]);
 
   function handleSubmit(event) {
-    console.log('submitted');
+    getFibSeq();
     var form = document.getElementById('fibForm');
     form.reset();
     event.preventDefault();
+  }
+
+  async function getFibSeq() {
+    const fibSeq = await fetch(`/api/fibonacci/${fibNum}`).then(response =>
+      response.json(),
+    );
+    setFibSeq(
+      fibSeq
+        ? fibSeq.join(', ')
+        : 'There was a problem getting the sequence from the server. You might have requested too many values.',
+    );
   }
 
   function onChange(event) {
@@ -17,12 +29,12 @@ export const FibInput = () => {
 
   return (
     <>
-      <form id="fibForm" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} id="fibForm">
         <label htmlFor="fib">How many Fibonacci digits do you want?</label>
         <input type="number" min="1" id="fib" onChange={onChange} />
         <input type="submit" value="Enter" />
       </form>
-      <p>Results</p>
+      <p>{fibSeq}</p>
     </>
   );
 };
