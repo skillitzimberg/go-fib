051f8e012d3e1585e388d4ce21c136824d3a387d
Scott Bergler <commandinghands@gmail.com>
2020-07-24 14:06:53 -0700

    Add event handlers.

--- client/src/FibInput.js
+++ client/src/FibInput.js
@@ -4,11 +4,22 @@ export const FibInput = () => {
   const [fibNum, setFibNum] = React.useState(0);
   const [fibSeq, setFibSeq] = React.useState([]);
 
+  function handleSubmit(event) {
+    console.log('submitted');
+    var form = document.getElementById('fibForm');
+    form.reset();
+    event.preventDefault();
+  }
+
+  function onChange(event) {
+    setFibNum(event.target.value);
+  }
+
   return (
     <>
-      <form id="fibForm">
+      <form id="fibForm" onSubmit={handleSubmit}>
         <label htmlFor="fib">How many Fibonacci digits do you want?</label>
-        <input type="number" min="1" id="fib" />
+        <input type="number" min="1" id="fib" onChange={onChange} />
         <input type="submit" value="Enter" />
       </form>
       <p>Results</p>
