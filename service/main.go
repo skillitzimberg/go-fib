a304b1438c4ca940ba00cdc9e0bad1a8f212ef31
Scott Bergler <commandinghands@gmail.com>
2020-07-24 13:34:00 -0700

    Handle api endpoints.

--- service/main.go
+++ service/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+// Index handles requests at "/api".
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+	fmt.Fprint(w, "Welcome!\nTo generate the Fibonacci sequence use the URL '/api/fibonnaci/{someNum} where someNum is an integer greater than 0.\n")
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+// Fibonacci handles requests at "/api/fibonacci/:number".
+func Fibonacci(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	num, err := strconv.Atoi(ps.ByName("number"))
+	check(err, "strconv.Atoi(ps.ByName('number')")
+
+	fib, err := fib(num)
+	check(err, "fib(num)")
+
+	fibJSON, err := json.Marshal(fib)
+	check(err, "json.Marshal(fib)")
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%v\n", string(fibJSON))
 }
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
+	router.GET("/hello/:name", Fibonacci)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
